6b3cea5363107f138c238cdf048c112390a0cdc2
gismitt <gundelhausmann@gmail.com>
2023-06-05 21:52:47 -0300

    feat(backend): fixing model User

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
+
 
 const userSchema = mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     tokens: [{
         type: mongoose.Schema.Types.ObjectId,
